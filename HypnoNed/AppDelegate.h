35ba44a6025f886c217c27b0fb5e591833a30d0c
cocos543 <543314602@qq.com>
2015-11-21 07:45:38 +0800

    Initial Commit

---
+++ HypnoNed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNed
+//
+//  Created by 郑克明 on 15/11/21.
+//  Copyright © 2015年 郑克明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HypnoNed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HypnoNed
+//
+//  Created by 郑克明 on 15/11/21.
+//  Copyright © 2015年 郑克明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
