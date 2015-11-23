c4c96f02f4eb3942d42d3f58de567675ee6c4ed4
cocos543 <543314602@qq.com>
2015-11-23 19:59:53 +0800

    注释调了颜色分页按钮.增加一个输入框,输入文字按回车键会随在屏幕上现实相应文字.

--- HypnoNed/HypnosisViewController.h
+++ HypnoNed/HypnosisViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface HypnosisViewController : UIViewController
+@interface HypnosisViewController : UIViewController <UITextFieldDelegate>
 
 
 @end
--- HypnoNed/HypnosisViewController.m
+++ HypnoNed/HypnosisViewController.m
@@ -8,6 +8,8 @@
 
 #import "HypnosisViewController.h"
 #import "HypnosisterView.h"
+
+
 @implementation HypnosisViewController
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
@@ -25,9 +27,51 @@
     [super loadView];
     HypnosisterView *backgrounddView = [[HypnosisterView alloc] init];
     self.view = backgrounddView;
+    
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hpnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.delegate = self;
+    [backgrounddView addSubview:textField];
+    
 }
 -(void)viewDidLoad{
     [super viewDidLoad];
     NSLog(@"Call viewDidLoad on HypnosisViewController");
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSLog(@"Pressing return.");
+    [self drawHypnoticMessage:textField.text];
+    return YES;
+}
+
+-(void)drawHypnoticMessage:(NSString *)message{
+    for(int i = 0; i < 20; i++){
+        UILabel *messageLabel = [[UILabel alloc] init];
+        
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor whiteColor];
+        messageLabel.text = message;
+        [messageLabel sizeToFit];
+        
+        int width = self.view.bounds.size.width - messageLabel.bounds.size.width;
+        width = arc4random() % width;
+        
+        int heigth = self.view.bounds.size.height - messageLabel.bounds.size.height;
+        heigth = arc4random() %heigth;
+        
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(width, heigth);
+        messageLabel.frame = frame;
+        [self.view addSubview:messageLabel];
+        
+        UIInterpolatingMotionEffect *motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @(-25);
+        motionEffect.maximumRelativeValue = @(25);
+        
+        [messageLabel addMotionEffect:motionEffect];
+    }
 }
 @end
